3a51822dabb318f93e09ff4c0ef0466e52e26a5c
whilewell <50164788+whilewell@users.noreply.github.com>
2023-03-17 09:13:01 +0800

    Update qk.sh

--- qk.sh
+++ qk.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 qjy_get_content() {
 	echo "start=========================="
 	cat $1 | awk -F ',' '{for(i=1;i<=NF;i++){ print $i}}' | grep "content\"\:" | awk -F ':' '{print $2}' > subtitles.txt.tmp
@@ -8,4 +10,46 @@ qjy_get_content() {
 
 qtag(){
     find `pwd` -name "*.c" -o -name "*.h" > ls.files && cscope -Rbkq -i ls.files && ctags -R
+}
+
+qc(){
+    cur_dir=`pwd`
+    cur_dir=${cur_dir%/*}
+    while true; do
+        if [ -d "$cur_dir/.repo" -o -d "$cur_dir/.git" ];then
+            cd $cur_dir
+            break 
+        fi
+        cur_dir=${cur_dir%/*}
+        [ "/home/xx" == "${cur_dir}" ] && break;
+    done
+}
+
+qpwd(){
+    cur_dir=`pwd`
+    echo "$cur_dir/$@"
+}
+
+qdiff(){
+    diff -r --no-dereference $@
+}
+
+qckjson(){
+    cat $1 | python -m json.tool
+}
+
+qtag(){
+    root_dir=`pwd`
+    find $root_dir -name "*.h" -o -name "*.c" > cscope.files
+    cscope -Rbkq -i cscope.files
+    ctags -R
+}
+
+qrsync(){
+    repo sync && repo start $RANDOM --all && repo manifest -ro tag.xml
+}
+
+qcl(){
+    local code
+    code=`cat ~/.codelist | fzf` && ${code}
 }
